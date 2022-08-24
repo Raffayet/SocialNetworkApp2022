c5f61c28aff316a579fcd193d2bf1b026d2f5d7c
IgorPavlov123 <igorpavlov106@gmail.com>
2022-08-24 12:27:57 +0200

    izmene

--- SocialNetwork/src/main/webapp/app/welcome.js
+++ SocialNetwork/src/main/webapp/app/welcome.js
@@ -9,13 +9,12 @@ Vue.component("welcome",{
 		<div class="d-flex align-items-center justify-content-center">
 			
 		 	<div style="marginTop:20px;">
-				  <div class="input-group mb-3">
+				  <div class="input-group mb-3" >
 					  <input v-model="searchInput" type="text" class="form-control" placeholder="Search users..." aria-label="Search..." aria-describedby="basic-addon2">
 					  <div class="input-group-append">
-					    <button  style="backgroundColor:#FF416C; borderRadius:10px;marginBottom:20px;" @click="$router.push('searchPage/' + searchInput)" class="btn btn-outline-primary" type="button">Search</button>
+					    <button  style="backgroundColor:#FF416C; borderRadius:10px;marginBottom:20px;" @click="$router.push('searchPage/' + searchInput)" class="btn btn-outline-primary" type="button">Searchddss</button>
 					  </div>
 					</div>
-				  <button style="backgroundColor:#FF416C; borderRadius:10px;" @click="$router.push('login')" type="button">Login</button>
 				 
 			</div>
 		</div>
