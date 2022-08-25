41cc660ca092920d20882747ffb4702e51ae200d
Raffayet <soviljnikola3@gmail.com>
2022-08-25 22:24:44 +0200

    uradjen dizajn feed page-a

--- SocialNetwork/src/main/webapp/app/feed.js
+++ SocialNetwork/src/main/webapp/app/feed.js
@@ -7,9 +7,23 @@ Vue.component("feed",{
     },
 	template:
 	`	
-		<div class="container">
-			<p>Dobro dosli</p>
-			<button style="backgroundColor:#FF416C; borderRadius:10px; marginTop:650px; marginLeft:590px;" v-on:click="logOut" type="button">Log Out</button>
+		<div class='root'>
+			<div style="display:flex;">
+				<div class="d-flex justify-content-center h-100">
+    				<div class="searchbar">
+      					<input class="search_input" type="text" name="" placeholder="Search...">
+      					<a href="#" class="search_icon"><i class="fas fa-search"></i></a>
+    				</div>
+    				<link rel="stylesheet" href="css/feed.css" type="text/css">
+					<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	    			<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+        		</div>
+        		<button style="backgroundColor:#5dbea3; height: 40px; padding:0 5px; border:#5dbea3; flexDirection:row; borderRadius:10px; marginBottom: 15px; " v-on:click="logOut" type="button">Edit Profile</button>
+				<button class="logout-button" style="backgroundColor:#FF416C; height: 40px; padding:0 5px; flexDirection:row; borderRadius:10px; marginBottom: 15px; marginLeft:30px;" v-on:click="logOut" type="button">Log Out</button>
+        	</div>
+			<div class="container" style="marginTop: -220px; width:1000px; background: transparent; background:rgba(1,1,1,0.75);">
+				<p style="color:white; fontSize:20px;">Dobro dosli</p>
+			</div>
 		</div>
 	`
 	,
